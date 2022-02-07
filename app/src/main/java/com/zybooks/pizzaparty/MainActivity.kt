d171aa738c409b305e21c2e6390cda8bc2fd1251
Jack Utter <78005286+JackDeetz@users.noreply.github.com>
2022-02-07 14:37:08 -0800

    Update MainActivity.kt
    Added KDoc Documentation

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -13,23 +13,47 @@ const val TAG = "MainActivity"
 const val SLICES_PER_PIZZA = 8
 
 
-
+/**
+ * This is a MainActivity class. It extends the AppCompatActivity class (an activity displays the UI and proccess User Input)
+ *
+ * This class defines the activity which starts when the app runs.
+ *
+ * @property [numAttendEditText] - lateinit (allows properties to be initiated in [onCreate]), UI for number of Pizza Eaters
+ * @property [numPizzasTextView] - ^ , text box to display the results of calculating how many pizzas to order (No UserInput)
+ * @property [howHungryRadioGroup] - ^, range of options for how hungry, one selection at a time
+ */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var numAttendEditText: EditText
     private lateinit var numPizzasTextView: TextView
     private lateinit var howHungryRadioGroup: RadioGroup
 
+    /**
+     * onCreate function (called when the activity is first created)
+     *
+     * @[setContentView] - sets the MainActivity content (properties) to the layout in activity_main.xml
+     * @[findViewById] - retrieves an object from the 'widget' from activity_main.xml
+     * @[Log.d] - creates a message on the system log (Useful for debugging)
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate was called")
+
         numAttendEditText = findViewById(R.id.num_attend_edit_text)
         numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
         howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
     }
 
+    /**
+     * calculateClick function ('Calculate' button on activity_main has been clicked)
+     * all information from 'numAttend' and 'slicesPerPerson' are used to calculate the totalPizzas value
+     * and update the numPizzaTextView
+     *
+     *
+     *
+     */
     fun calculateClick(view: View) {
 
         Log.d(TAG, "button was called")
